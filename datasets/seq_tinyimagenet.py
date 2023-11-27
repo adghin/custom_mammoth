85b55d31f395da9627ea9a72857082b1664e2a77
adghin <adrian.ghinea00@gmail.com>
2023-11-27 17:06:59 +0100

    Update seq_tinyimagenet.py

--- datasets/seq_tinyimagenet.py
+++ datasets/seq_tinyimagenet.py
@@ -188,6 +188,9 @@ class SequentialTinyImagenet(ContinualDataset):
         #To use this argument add the same in utils/args.py --> add_management_args
         parser.add_argument('--optim_upscale',type=int,help='Upscale images to default size of pre-trained model recipe. 0 (no upscale), 1 (upscale)',default=0,choices=[0,1])
         parser.add_argument('--backbone',type=str,help='Pre-trained backbone to use, choose from pytorch models: resnet18, resnet34, resnet50, resnet101, resnet152, vit_b_16, vit_b_32', default='resnet18')
+
+        args = parser.parse_args()
+        return args
     ### END   --- aghinea
     
     @staticmethod

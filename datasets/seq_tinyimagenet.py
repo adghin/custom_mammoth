a20feacd15ef29a174cdf791d10f0b2fa6e57e27
adghin <adrian.ghinea00@gmail.com>
2023-11-27 20:40:55 +0100

    Update seq_tinyimagenet.py

--- datasets/seq_tinyimagenet.py
+++ datasets/seq_tinyimagenet.py
@@ -7,7 +7,7 @@ import os
 from typing import Optional
 
 ### START --- aghinea
-from argparse import ArgumentParser
+from utils.main import parse_args
 ### END   --- aghinea
 
 import numpy as np
@@ -138,7 +138,7 @@ class SequentialTinyImagenet(ContinualDataset):
     N_CLASSES_PER_TASK = 20
     N_TASKS = 10
 
-    args = parse_args()
+    args = main.parse_args()
     if args.optim_upscale == 1:
         if backbone == 'resnet18' or backbone == 'vit_b_16' or backbone == 'vit_b_32':
             image_resize = 256

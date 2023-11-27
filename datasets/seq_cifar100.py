cf136e76b0310dd4dc2790a0440d34384ec585c9
adghin <adrian.ghinea00@gmail.com>
2023-11-27 16:48:21 +0100

    Update seq_cifar100.py

--- datasets/seq_cifar100.py
+++ datasets/seq_cifar100.py
@@ -69,16 +69,38 @@ class SequentialCIFAR100(ContinualDataset):
     N_CLASSES_PER_TASK = 10
     N_TASKS = 10
     
-    TRANSFORM = transforms.Compose(
-            [transforms.RandomCrop(32, padding=4),
-             transforms.RandomHorizontalFlip(),
-             transforms.ToTensor(),
-             transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))])
-  
-    TEST_TRANSFORM = transforms.Compose([
-                            transforms.ToTensor(),
-                            transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))
-                            ])
+    if args.optim_upscale == 1:
+        if backbone == 'resnet18' or backbone == 'vit_b_16' or backbone == 'vit_b_32':
+            image_resize = 256
+        else:
+            image_resize = 232
+
+        image_crop       = 224
+
+        TRANSFORM = transforms.Compose(
+                                      [transforms.Resize(image_resize, interpolation=transforms.InterpolationMode.BILINEAR),
+                                       transforms.RandomCrop(image_crop),
+                                       transforms.RandomHorizontalFlip(),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))])
+
+        TEST_TRANSFORM = transforms.Compose(
+                                      [transforms.Resize(image_resize, interpolation=transforms.InterpolationMode.BILINEAR),
+                                       transforms.CentralCrop(image_crop),
+                                       transforms.RandomHorizontalFlip(),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))])    
+    else:
+        TRANSFORM = transforms.Compose(
+                              [transforms.RandomCrop(32, padding=4),
+                               transforms.RandomHorizontalFlip(),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))])
+      
+        TEST_TRANSFORM = transforms.Compose([
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))
+                                ])
   
     def get_examples_number(self):
         train_dataset = MyCIFAR100(base_path() + 'CIFAR10', train=True,

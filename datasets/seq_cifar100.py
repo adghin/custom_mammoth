20712fe99302a96b4b29fc9749cd94305f29b5ae
adghin <adrian.ghinea00@gmail.com>
2023-11-27 17:02:23 +0100

    Update seq_cifar100.py

--- datasets/seq_cifar100.py
+++ datasets/seq_cifar100.py
@@ -72,7 +72,8 @@ class SequentialCIFAR100(ContinualDataset):
     SETTING = 'class-il'
     N_CLASSES_PER_TASK = 10
     N_TASKS = 10
-    
+
+    args = parse_args()
     if args.optim_upscale == 1:
         if backbone == 'resnet18' or backbone == 'vit_b_16' or backbone == 'vit_b_32':
             image_resize = 256

327a98dded08792ebfce46c94b9b382dc840726b
adghin <adrian.ghinea00@gmail.com>
2023-11-27 22:00:46 +0100

    Update seq_tinyimagenet.py

--- datasets/seq_tinyimagenet.py
+++ datasets/seq_tinyimagenet.py
@@ -142,7 +142,7 @@ class SequentialTinyImagenet(ContinualDataset):
 
         TEST_TRANSFORM = transforms.Compose(
                                       [transforms.Resize(image_resize, interpolation=transforms.InterpolationMode.BILINEAR),
-                                       transforms.CentralCrop(image_crop),
+                                       transforms.CenterCrop(image_crop),
                                        transforms.RandomHorizontalFlip(),
                                        transforms.ToTensor(),
                                        transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))])    

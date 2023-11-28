cb8a24b703f3f79f71069ed3a01a238245ad99db
adghin <adrian.ghinea00@gmail.com>
2023-11-28 21:43:45 +0100

    Update seq-cifar100-v1.py

--- datasets/seq-cifar100-v1.py
+++ datasets/seq-cifar100-v1.py
@@ -81,7 +81,6 @@ class SequentialCIFAR100(ContinualDataset):
     TEST_TRANSFORM = transforms.Compose(
                                   [transforms.Resize(256, interpolation=transforms.InterpolationMode.BILINEAR),
                                    transforms.CenterCrop(224),
-                                   transforms.RandomHorizontalFlip(),
                                    transforms.ToTensor(),
                                    transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))])    
 

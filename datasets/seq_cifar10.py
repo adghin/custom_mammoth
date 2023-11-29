5ac3a18079f9a73b7abfe2920dd76099b92c8e17
adghin <adrian.ghinea00@gmail.com>
2023-11-29 13:06:18 +0100

    Update seq_cifar10.py

--- datasets/seq_cifar10.py
+++ datasets/seq_cifar10.py
@@ -85,6 +85,8 @@ class SequentialCIFAR10(ContinualDataset):
       
         transform = self.TRANSFORM
         test_transform = self.TEST_TRANSFORM
+        print(transform)
+        print(test_transform)
 
         train_dataset = MyCIFAR10(base_path() + 'CIFAR10', train=True,
                                   download=True, transform=transform)

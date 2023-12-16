47ad01d286cef88d900604e3a140086ea47ec9b6
adghin <adrian.ghinea00@gmail.com>
2023-12-16 18:23:10 +0100

    Update training.py

--- utils/training.py
+++ utils/training.py
@@ -108,8 +108,8 @@ def train(model: ContinualModel, dataset: ContinualDataset,
         wandb.init(dir='/home/aghinea/tmp/', project=project, entity=args.wandb_entity, config=vars(args))
         args.wandb_url = wandb.run.get_url()
 
-    print(model)
-    log_model = {'Model': model}
+    
+    log_model = {'Model': tuple(model)}
     wandb.log({log_model})
 
     model.net.to(model.device)

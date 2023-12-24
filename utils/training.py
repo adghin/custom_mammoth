41815cb2d1592d992794b29c03dd254d2193392e
adghin <adrian.ghinea00@gmail.com>
2023-12-24 12:50:38 +0100

    Update training.py

--- utils/training.py
+++ utils/training.py
@@ -24,6 +24,7 @@ except ImportError:
 ###START --- aghinea
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
 
 def static_vars(**kwargs):
     def decorate(func):

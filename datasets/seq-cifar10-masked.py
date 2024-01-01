b5d6ba2431a534ebbdad496f19e565f9cc1bc16c
adghin <adrian.ghinea00@gmail.com>
2024-01-01 21:36:07 +0100

    Create seq-cifar10-masked.py

---
+++ datasets/seq-cifar10-masked.py
@@ -0,0 +1,165 @@
+# Copyright 2022-present, Lorenzo Bonicelli, Pietro Buzzega, Matteo Boschini, Angelo Porrello, Simone Calderara.
+# All rights reserved.
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+from typing import Tuple
+
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from backbone.ResNet18 import resnet18
+from PIL import Image
+from torchvision.datasets import CIFAR10
+
+from datasets.seq_tinyimagenet import base_path
+from datasets.transforms.denormalization import DeNormalize
+from datasets.utils.continual_dataset import (ContinualDataset,
+                                              store_masked_loaders,dataloader_without_mask)
+from datasets.utils.validation import get_train_val
+
+class TCIFAR10(CIFAR10):
+    """Workaround to avoid printing the already downloaded messages."""
+    def __init__(self, root, train=True, transform=None,
+                 target_transform=None, download=False) -> None:
+        self.root = root
+        super(TCIFAR10, self).__init__(root, train, transform, target_transform, download=not self._check_integrity())
+
+class MyCIFAR10(CIFAR10):
+    """
+    Overrides the CIFAR10 dataset to change the getitem function.
+    """
+    def __init__(self, root, train=True, transform=None,
+                 target_transform=None, download=False) -> None:
+        self.not_aug_transform = transforms.Compose([transforms.ToTensor()])
+        self.root = root
+        super(MyCIFAR10, self).__init__(root, train, transform, target_transform, download=not self._check_integrity())
+
+    def __getitem__(self, index: int) -> Tuple[Image.Image, int, Image.Image]:
+        """
+        Gets the requested element from the dataset.
+        :param index: index of the element to be returned
+        :returns: tuple: (image, target) where target is index of the target class.
+        """
+        img, target = self.data[index], self.targets[index]
+
+        # to return a PIL Image
+        img = Image.fromarray(img, mode='RGB')
+        original_img = img.copy()
+
+        not_aug_img = self.not_aug_transform(original_img)
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        if hasattr(self, 'logits'):
+            return img, target, not_aug_img, self.logits[index]
+
+        return img, target, not_aug_img
+
+
+class SequentialCIFAR10(ContinualDataset):
+    NAME = 'seq-cifar10'
+    SETTING = 'class-il'
+    N_CLASSES_PER_TASK = 2
+
+    N_TASKS = 4
+                        
+    TRANSFORM = transforms.Compose(
+                          [transforms.RandomCrop(32, padding=4),
+                           transforms.RandomHorizontalFlip(),
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))])
+  
+    TEST_TRANSFORM = transforms.Compose(
+                          [transforms.ToTensor(),
+                           transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))])
+  
+    def get_data_loaders(self):      
+        transform = self.TRANSFORM
+        test_transform = self.TEST_TRANSFORM
+  
+        train_dataset = MyCIFAR10(base_path() + 'CIFAR10', train=True,
+                                  download=True, transform=transform)
+        if self.args.validation:
+            train_dataset, test_dataset = get_train_val(train_dataset,
+                                                    test_transform, self.NAME)
+        else:
+            test_dataset = TCIFAR10(base_path() + 'CIFAR10',train=False,
+                                   download=True, transform=test_transform)
+
+        train, test = store_masked_loaders(train_dataset, test_dataset, self)
+        return train, test
+
+    ###START --- aghinea
+    def get_args(self):
+        """
+        Get args from namespace
+        """
+        args = super().get_args()
+        return args
+
+    @classmethod
+    def change_transform(cls,backbone):
+        image_crop   = 224
+        if(backbone == 'resnet18' or backbone == 'vit_b_16' or backbone == 'vit_b_32'):
+            image_resize = 256
+        else:  #backbone == 'resnet50' or 'resnet152'
+            image_resize = 232
+            
+        cls.TRANSFORM = transforms.Compose(
+                          [transforms.Resize(image_resize, interpolation=transforms.InterpolationMode.BILINEAR),
+                           transforms.RandomCrop(image_crop),
+                           transforms.RandomHorizontalFlip(),
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))])
+      
+        cls.TEST_TRANSFORM = transforms.Compose(
+                              [transforms.Resize(image_resize, interpolation=transforms.InterpolationMode.BILINEAR),
+                               transforms.CenterCrop(image_crop),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))])
+    #END     --- aghinea
+
+    @staticmethod
+    def get_transform():
+        transform = transforms.Compose(
+            [transforms.ToPILImage(), SequentialCIFAR10.TRANSFORM])
+        return transform
+
+    @staticmethod
+    def get_backbone():
+        return resnet18(SequentialCIFAR10.N_CLASSES_PER_TASK
+                        * SequentialCIFAR10.N_TASKS)
+
+    @staticmethod
+    def get_loss():
+        return F.cross_entropy
+
+    @staticmethod
+    def get_normalization_transform():
+        transform = transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))
+        return transform
+
+    @staticmethod
+    def get_denormalization_transform():
+        transform = DeNormalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))
+        return transform
+
+    @staticmethod
+    def get_scheduler(model, args):
+        return None
+
+    @staticmethod
+    def get_epochs():
+        return 50
+
+    @staticmethod
+    def get_batch_size():
+        return 32
+
+    @staticmethod
+    def get_minibatch_size():
+        return SequentialCIFAR10.get_batch_size()
